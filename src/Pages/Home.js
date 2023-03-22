08cd0bef7df4188cf556be14d034d9f9512176bf
jaceruth <jacemruth@gmail.com>
2023-03-21 22:33:29 -0500

    Question blank

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -6,12 +6,19 @@ import React from "react";
 // import Footer from "../components/Navigation/Footer";
 // import AboutUs from "../components/HomePage/AboutUs";
 // import LoginPage from "../components/LoginPage/LoginPage";
-import SignupPage from "../components/LoginPage/SignupPage";
+//import SignupPage from "../components/LoginPage/SignupPage";
+import MultipleChoice from "../components/Questions/MultipleChoice/MultipleChoice";
+import athena from "../components/Questions/images/athena.png"
 
 function Home() {
   return (
     <div>
-      <SignupPage />
+      <MultipleChoice
+        questionText="What type of dog do you have?"
+        imageSrc={athena}
+        answerOptions={["Golden", "Golden Retriever", "German Shepherd", "Cat"]}
+      />
+      {/* <SignupPage /> */}
       {/* <Header />
       <Hero />
       <Features />
---
+++ src/components/Questions/MultipleChoice/MultipleChoice.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import "./MultipleChoice.css"
+import athena from "../images/athena.png"
+
+function MultipleChoiceQuestion(props) {
+  const [selectedOption, setSelectedOption] = useState(null);
+  
+  const handleOptionChange = (option) => {
+    setSelectedOption(option);
+  };
+
+  return (
+    <div className="question-container">
+      <img src={props.imageSrc} alt="Question image" />
+      <h2>{props.questionText}</h2>
+      <ul>
+        {props.answerOptions.map((option, index) => (
+          <li key={index}>
+            <label>
+              <input
+                type="radio"
+                value={option}
+                checked={selectedOption === option}
+                onChange={() => handleOptionChange(option)}
+              />
+              {option}
+            </label>
+          </li>
+        ))}
+      </ul>
+      <button type="submit" className="submit-button">
+        Submit
+      </button>
+    </div>
+  );
+}
+
+export default MultipleChoiceQuestion;
