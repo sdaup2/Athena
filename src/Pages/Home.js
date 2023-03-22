44d0e5815d2120439efb9fdcff5835aacad08412
Ealynn Hsu <89547630+ehsu3@users.noreply.github.com>
2023-03-21 22:39:21 -0500

    cleaned up the home file :')

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,41 +1,26 @@
 import React from "react";
-<<<<<<< Updated upstream
-// import Header from "../components/Navigation/Header";
-// import Hero from "../components/HomePage/Hero";
-// import Features from "../components/HomePage/Features";
-// import Testimonials from "../components/HomePage/Testimonials";
-// import Footer from "../components/Navigation/Footer";
-// import AboutUs from "../components/HomePage/AboutUs";
-// import LoginPage from "../components/LoginPage/LoginPage";
-//import SignupPage from "../components/LoginPage/SignupPage";
-import MultipleChoice from "../components/Questions/MultipleChoice/MultipleChoice";
-import athena from "../components/Questions/images/athena.png"
-=======
 import Header from "../components/Navigation/Header";
 import Hero from "../components/HomePage/Hero";
 import Features from "../components/HomePage/Features";
 import Testimonials from "../components/HomePage/Testimonials";
 import Footer from "../components/Navigation/Footer";
- import AboutUs from "../components/HomePage/AboutUs";
+import AboutUs from "../components/HomePage/AboutUs";
 import LoginPage from "../components/LoginPage/LoginPage";
 import SignupPage from "../components/LoginPage/SignupPage";
->>>>>>> Stashed changes
+import MultipleChoice from "../components/Questions/MultipleChoice/MultipleChoice";
+import athena from "../components/Questions/images/athena.png"
 
 function Home() {
   return (
     <div>
-<<<<<<< Updated upstream
-      <MultipleChoice
+      {/* <MultipleChoice
         questionText="What type of dog do you have?"
         imageSrc={athena}
         answerOptions={["Golden", "Golden Retriever", "German Shepherd", "Cat"]}
       />
-      {/* <SignupPage /> */}
-      {/* <Header />
-=======
-      {/* <SignupPage /> */}
+      <SignupPage /> */}
+      // Main page elements are below
       <Header />
->>>>>>> Stashed changes
       <Hero />
       <Features />
       <Testimonials />
