1877221e3be485ee0681f6a0aff134c99c35653d
bbode27 <89547668+bbode27@users.noreply.github.com>
2023-04-19 08:49:08 -0500

    does stuff when clicking on icon

--- client/src/components/ClassIcon/ClassIcon.js
+++ client/src/components/ClassIcon/ClassIcon.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './ClassIcon.css';
 import image from "../Navigation/athenalogo.png";
+import { socket } from '../../socket';
+import { getAuth } from 'firebase/auth';
+import { useNavigate } from 'react-router-dom';
+
+
 
 function ClassComponent(props) {
+
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const navigate = useNavigate();
+  let uid = null;
+  if (user !== null) {
+    uid = user.uid;
+  }
+
+  function ClassIconHandler() {
+    socket.emit("clicked on class", uid, props.name);
+    console.log("asked back end for clases");
+    navigate("/");
+  }
+
+
+
   return (
     <div className="class-component">
       <button className="class-button">
-        <div className="class-icon" style={{ backgroundImage: `url(${image})` }}>
+        <div className="class-icon" onClick = {ClassIconHandler} style={{ backgroundImage: `url(${image})` }}>
           {props.iconText}
         </div>
         <div className="class-name">{props.name}</div>
@@ -15,5 +37,7 @@ function ClassComponent(props) {
     </div>
   );
 }
+
+
 
 export default ClassComponent;
