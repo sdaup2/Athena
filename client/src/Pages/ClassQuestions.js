6fa9997735335c305f85b5a3b249c2cb5d782ecd
sdaup2 <sdaup2@huskers.unl.edu>
2023-04-19 12:39:10 -0500

    Update ClassQuestions.js

--- client/src/Pages/ClassQuestions.js
+++ client/src/Pages/ClassQuestions.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ClassQuestionSets from "../components/ClassQuestionsSets/ClassQuestionSets";
 import Footer from "../components/Navigation/Footer";
-import Header from '../components/Navigation/Header';
+import Header from "../components/Navigation/Header";
 import { socket } from "../socket";
 
-socket.on("QS info", async(qsNames, class_code, class_name) => {
+socket.on("QS info", async (qsNames, class_code, class_name) => {
   classes.at(1).classCode = class_code;
   classes.at(1).className = class_name;
   console.log(classes.at(1).classCode);
@@ -15,31 +15,34 @@ socket.on("QS info", async(qsNames, class_code, class_name) => {
 let classes = [
   {
     id: 1,
-    className: "Mathematics",
-    classCode: "MATH101",
-    questionSets: [
-      { id: 1, name: "Algebra" },
-      { id: 2, name: "Calculus" },
-      { id: 3, name: "Geometry" },
-    ],
+    className: "Class Name goes HERE", // CLASS NAME
+    classCode: "Class Code goes HERE", // CLASS CODE
+    questionSets:
+      //THIS IS WHERE YOU PUT THE LIST OF QUESTION SETS FOR CURRENT CLASS
+
+      [
+        { id: 4, name: "Question set name goes HERE" },
+        { id: 2, name: "Calculus" },
+        { id: 3, name: "Geometry" },
+      ],
   },
 ];
 
 const Page = () => {
   return (
     <div>
-    <Header />
-    <div>
-      {classes.map((classObj) => (
-        <ClassQuestionSets
-          key={classObj.id}
-          className={classObj.className}
-          classCode={classObj.classCode}
-          questionSets={classObj.questionSets}
-        />
-      ))}
-    </div>
-    <Footer />
+      <Header />
+      <div>
+        {classes.map((classObj) => (
+          <ClassQuestionSets
+            key={classObj.id}
+            className={classObj.className}
+            classCode={classObj.classCode}
+            questionSets={classObj.questionSets}
+          />
+        ))}
+      </div>
+      <Footer />
     </div>
   );
 };
