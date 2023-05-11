6886b796163f907a08c43cafee655710d43569c5
bbode27 <89547668+bbode27@users.noreply.github.com>
2023-05-11 17:37:05 -0500

    updated to match seth

--- client/src/components/Questions/MultipleChoiceMain.js
+++ client/src/components/Questions/MultipleChoiceMain.js
@@ -3,11 +3,17 @@ import MultipleChoice from "./MultipleChoice/MultipleChoice";
 import Footer from "../Navigation/Footer";
 import Header from "../Navigation/Header";
 import athena from "../../assets/athena.png";
-import { socket } from "../../socket";
-
-
+// import { socket } from “../socket”;
+// import { getAuth } from “firebase/auth”;
 function MultipleChoiceMain() {
+  const [question, setQuestion] = useState([]);
+  const getQuestion = () => {};
 
+
+  //these are what you change brooke
+  const questionString = "HARD 1";
+  const imageConst = athena;
+  const answerOptionsString = ["HARD 2", "HARD 3", "HARD 4"];
 
   return (
     <div>
@@ -15,14 +21,13 @@ function MultipleChoiceMain() {
       <div>
         <h1>MultipleChoice</h1>
         <MultipleChoice
-          questionText="HARD CODED"
-          imageSrc={athena}
-          answerOptions={["HARD", "CODED", "HARD", "CODED"]}
+          questionText={questionString}
+          imageSrc={imageConst}
+          answerOptions={answerOptionsString}
         />
       </div>
       <Footer />
     </div>
   );
 }
-
 export default MultipleChoiceMain;
