d68905075cdb20fe514ae3aa63266c6018b62475
sdaup2 <sdaup2@huskers.unl.edu>
2023-05-13 15:22:00 -0500

    cleanup unused code
    deleted commented out stuff

--- client/src/pages/HomePage/Home.js
+++ client/src/pages/HomePage/Home.js
@@ -3,20 +3,10 @@ import Header from "../../components/Navigation/Header";
 import Welcome from "./Welcome/Welcome";
 import Features from "./Features/Features";
 import Footer from "../../components/Navigation/Footer";
-// import LoginPage from "../components/LoginPage/LoginPage";
-// import SignupPage from "../components/LoginPage/SignupPage";
-// import MultipleChoice from "../components/Questions/MultipleChoice/MultipleChoice";
-// import athena from "../components/Questions/images/athena.png";
 
 function Home() {
   return (
     <div>
-      {/* <MultipleChoice
-        questionText="What type of dog do you have?"
-        imageSrc={athena}
-        answerOptions={["Lab", "Golden Retriever", "German Shepherd", "Cat"]}
-      /> */}
-      {/* <SignupPage />  */}
       <Header />
       <Welcome />
       <Features />
